7f56d764afe2b13e91aa16ebc989aa2d67dac9dd
kushvinay <kushvinay19@gmail.com>
2024-09-12 15:52:07 +0530

    add baseurl

--- axiosconfig.js
+++ axiosconfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:4040/",
+    baseURL: "https://backend-2-k1g1.onrender.com",
     withCredentials: true,
 });
 
